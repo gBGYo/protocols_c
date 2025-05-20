ca0dcee394317af1a60aa3edb4df059c0d393275
Sergei Zhilyakov <zhilyakov.bs@gmail.com>
2025-05-20 17:55:09 +0300

    Add lab1

---
+++ labs/lab1/main.c
@@ -0,0 +1,225 @@
+#include "kuznyechik.h"
+#include "streebog.h"
+#include "log.h"
+#include <pwd.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+
+int check_exe_integrity(char *exe_path)
+{
+    FILE *digest_fp = fopen("./lab1.digest", "rb");
+    if (digest_fp == NULL)
+    {
+        log_error("не удается открыть файл lab1.digest");
+        return 1;
+    }
+    uint8_t digest_buf[32] = {0};
+    if (fread(digest_buf, 1, 32, digest_fp) != 32)
+    {
+        log_error("не удается прочитать содержимое файла lab1.digest");
+        return 1;
+    }
+    fseek(digest_fp, 0, SEEK_SET);
+
+    FILE *exe_fp = fopen(exe_path, "rb");
+    if (exe_fp == NULL)
+    {
+        log_error("не удается открыть файл %s", exe_path);
+        return 1;
+    }
+    fseek(exe_fp, 0, SEEK_END);
+    size_t exe_len = ftell(exe_fp);
+    fseek(exe_fp, 0, SEEK_SET);
+    uint8_t *exe_buf = (uint8_t *)malloc(sizeof(uint8_t) * exe_len);
+    if (fread(exe_buf, 1, exe_len, exe_fp) != exe_len)
+    {
+        log_error("не удается прочитать содержимое файла %s: %s", exe_path, strerror(errno));
+        return 1;
+    }
+    fseek(exe_fp, 0, SEEK_SET);
+
+    uint8_t exe_digest_buf[32] = {0};
+    Streebog sb = {0};
+    streebog_new(&sb);
+    streebog_hash_array(&sb, exe_buf, exe_len, exe_digest_buf);
+
+    for (size_t i = 0; i < 32; i++)
+    {
+        if (exe_digest_buf[i] != digest_buf[i])
+        {
+            log_warn("хэш-сумма расходится в байтовой позиции i=%ld", i);
+            return 1;
+        }
+    }
+
+    streebog_clear(&sb);
+    free(exe_buf);
+    fclose(exe_fp);
+    fclose(digest_fp);
+    return 0;
+}
+
+int check_valid_user(struct passwd *pw)
+{
+    FILE *user_db_fp = fopen("./users.db", "rb");
+    if (user_db_fp == NULL)
+    {
+        log_error("не удается открыть файл users.db");
+        return 1;
+    }
+
+    uint8_t username_digest[32] = {0};
+    Streebog sb = {0};
+    streebog_new(&sb);
+    streebog_hash_array(&sb, (uint8_t *)pw->pw_name, strlen(pw->pw_name), username_digest);
+
+    fseek(user_db_fp, 0, SEEK_END);
+    uint32_t user_db_len = ftell(user_db_fp) / 32;
+    fseek(user_db_fp, 0, SEEK_SET);
+    for (size_t i = 0; i < user_db_len; i++)
+    {
+        uint8_t tmp_digest[32] = {0};
+        if (fread(tmp_digest, 1, 32, user_db_fp) != 32)
+        {
+            log_error("не удается прочитать запись в файле users.db");
+            streebog_clear(&sb);
+            fclose(user_db_fp);
+            return 1;
+        }
+        for (size_t j = 0; j < 32; j++)
+        {
+            if (username_digest[j] != tmp_digest[j])
+            {
+                goto next_entry;
+            }
+        }
+
+        log_info("учетная запись `%s` получила доступ", pw->pw_name);
+        streebog_clear(&sb);
+        fclose(user_db_fp);
+        return 0;
+    next_entry:
+    }
+
+    log_warn("неизвестная учетная запись `%s` попыталась получить доступ", pw->pw_name);
+    streebog_clear(&sb);
+    fclose(user_db_fp);
+    return 1;
+}
+
+int check_key_expiration(uint8_t key[32])
+{
+    FILE *key_fp = fopen("./key.db", "r");
+    if (key_fp == NULL)
+    {
+        log_error("не удается открыть файл key.db");
+        return 1;
+    }
+    if (fread(key, 1, 32, key_fp) != 32)
+    {
+        log_error("не удается прочитать содержимое файла key.db");
+        fclose(key_fp);
+        return 1;
+    }
+
+    time_t expire = 0;
+    if (fread(&expire, 1, sizeof(time_t), key_fp) != sizeof(time_t))
+    {
+        log_error("не удается прочитать содержимое файла key.db");
+        kuz_clear_buf(key, 32);
+        fclose(key_fp);
+        return 1;
+    }
+
+    time_t now = time(NULL);
+    if (now >= expire)
+    {
+        log_error("срок действия ключа истек в %ld; текущее время %ld", expire, now);
+        kuz_clear_buf(key, 32);
+        fclose(key_fp);
+        return 1;
+    }
+
+    fclose(key_fp);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        printf("USAGE: %s <file_in> <file_out>\n", argv[0]);
+        return 1;
+    }
+    FILE *log_fp = fopen("./lab1.log", "a+");
+    if (log_add_fp(log_fp, LOG_TRACE) < 0)
+    {
+        fprintf(stderr, "не удается открыть файл lab1.log\n");
+        return 1;
+    }
+
+    if (check_exe_integrity(argv[0]) != 0)
+    {
+        fclose(log_fp);
+        return 2;
+    }
+
+    struct passwd *pw = getpwuid(geteuid());
+    if (pw == NULL)
+    {
+        log_error("не удается получить информацию об учетной записе пользователя с помощью getpwuid()");
+        fclose(log_fp);
+        return 1;
+    }
+
+    if (check_valid_user(pw) != 0)
+    {
+        fclose(log_fp);
+        return 3;
+    }
+
+    uint8_t key[32] = {0};
+    if (check_key_expiration(key) != 0)
+    {
+        fclose(log_fp);
+        return 4;
+    }
+
+    FILE *in_fp = fopen(argv[1], "rb");
+    if (in_fp == NULL)
+    {
+        log_error("не удается открыть файл %s", argv[1]);
+        kuz_clear_buf(key, 32);
+        fclose(log_fp);
+        return 1;
+    }
+    fseek(in_fp, 0, SEEK_END);
+    log_info("файл %s открыт на чтение; размер: %ld байт", argv[1], ftell(in_fp));
+    fseek(in_fp, 0, SEEK_SET);
+
+    FILE *out_fp = fopen(argv[2], "w+b");
+    if (out_fp == NULL)
+    {
+        log_error("не удается открыть файл %s", argv[2]);
+        kuz_clear_buf(key, 32);
+        fclose(log_fp);
+        return 1;
+    }
+    fseek(out_fp, 0, SEEK_END);
+    log_info("файл %s открыт на запись; размер: %ld байт", argv[2], ftell(out_fp));
+    fseek(out_fp, 0, SEEK_SET);
+
+    Kuznyechik kuz = {0};
+    kuz_new(&kuz, key);
+    kuz_ofb_encrypt(in_fp, out_fp, &kuz);
+
+    log_info("успешно зашифровано %ld байт", ftell(out_fp));
+
+    fclose(in_fp);
+    fclose(out_fp);
+    kuz_clear(&kuz);
+    kuz_clear_buf(key, 32);
+    fclose(log_fp);
+    return 0;
+}
---
+++ labs/lab1/test.c
@@ -0,0 +1,233 @@
+#include "kuznyechik.h"
+#include "kdf_tree_gostr3411_2012_256.h"
+
+const char *file_1Mb_in = "./in/1Mb";
+const char *file_1Mb_out = "./out/1Mb";
+const char *file_1Mb_dec = "./dec/1Mb";
+
+const char *file_100Mb_in = "./in/100Mb";
+const char *file_100Mb_out = "./out/100Mb";
+const char *file_100Mb_dec = "./dec/100Mb";
+
+const char *file_1000Mb_in = "./in/1000Mb";
+const char *file_1000Mb_out = "./out/1000Mb";
+const char *file_1000Mb_dec = "./dec/1000Mb";
+
+const char *file_mil_blocks_in = "./in/mil_blocks";
+const char *file_mil_blocks_out = "./out/mil_blocks";
+const char *file_mil_blocks_dec = "./dec/mil_blocks";
+
+void encrypt_1Mb()
+{
+    const char *path_in = file_1Mb_in;
+    const char *path_out = file_1Mb_out;
+    const char *path_dec = file_1Mb_dec;
+
+    FILE *f_in = fopen(path_in, "rb");
+    if (f_in == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_in);
+        perror("fopen");
+        exit(1);
+    }
+    FILE *f_out = fopen(path_out, "rwb");
+    if (f_out == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_out);
+        perror("fopen");
+        exit(1);
+    }
+    FILE *f_dec = fopen(path_dec, "wb");
+    if (f_dec == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_dec);
+        perror("fopen");
+        exit(1);
+    }
+
+    kuz_key_t key = {
+        0x83, 0xf6, 0x1e, 0x13, 0xde, 0x22, 0x50, 0x51,
+        0x6c, 0x80, 0x53, 0xc3, 0xc2, 0xea, 0x92, 0x63,
+        0xf5, 0x1d, 0x17, 0xae, 0xdc, 0x37, 0xd3, 0x7e,
+        0x51, 0x54, 0x52, 0xb1, 0xfe, 0xf8, 0x04, 0x32};
+    Kuznyechik kuz;
+    kuz_new(&kuz, key);
+
+    kuz_ofb_encrypt(f_in, f_out, &kuz);
+    fseek(f_out, 0, SEEK_SET);
+
+    kuz_clear(&kuz);
+    kuz_new(&kuz, key);
+
+    kuz_ofb_encrypt(f_out, f_dec, &kuz);
+
+    kuz_clear(&kuz);
+    fclose(f_dec);
+    fclose(f_out);
+    fclose(f_in);
+}
+
+void encrypt_100Mb()
+{
+    const char *path_in = file_100Mb_in;
+    const char *path_out = file_100Mb_out;
+    const char *path_dec = file_100Mb_dec;
+
+    FILE *f_in = fopen(path_in, "rb");
+    if (f_in == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_in);
+        perror("fopen");
+        exit(1);
+    }
+    FILE *f_out = fopen(path_out, "rwb");
+    if (f_out == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_out);
+        perror("fopen");
+        exit(1);
+    }
+    FILE *f_dec = fopen(path_dec, "wb");
+    if (f_dec == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_dec);
+        perror("fopen");
+        exit(1);
+    }
+
+    kuz_key_t key = {
+        0x83, 0xf6, 0x1e, 0x13, 0xde, 0x22, 0x50, 0x51,
+        0x6c, 0x80, 0x53, 0xc3, 0xc2, 0xea, 0x92, 0x63,
+        0xf5, 0x1d, 0x17, 0xae, 0xdc, 0x37, 0xd3, 0x7e,
+        0x51, 0x54, 0x52, 0xb1, 0xfe, 0xf8, 0x04, 0x32};
+    Kuznyechik kuz;
+    kuz_new(&kuz, key);
+
+    kuz_ofb_encrypt(f_in, f_out, &kuz);
+    fseek(f_out, 0, SEEK_SET);
+
+    kuz_clear(&kuz);
+    kuz_new(&kuz, key);
+
+    kuz_ofb_encrypt(f_out, f_dec, &kuz);
+
+    kuz_clear(&kuz);
+    fclose(f_dec);
+    fclose(f_out);
+    fclose(f_in);
+}
+
+void encrypt_1000Mb()
+{
+    const char *path_in = file_1000Mb_in;
+    const char *path_out = file_1000Mb_out;
+    const char *path_dec = file_1000Mb_dec;
+
+    FILE *f_in = fopen(path_in, "rb");
+    if (f_in == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_in);
+        perror("fopen");
+        exit(1);
+    }
+    FILE *f_out = fopen(path_out, "rwb");
+    if (f_out == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_out);
+        perror("fopen");
+        exit(1);
+    }
+    FILE *f_dec = fopen(path_dec, "wb");
+    if (f_dec == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_dec);
+        perror("fopen");
+        exit(1);
+    }
+
+    kuz_key_t key = {
+        0x83, 0xf6, 0x1e, 0x13, 0xde, 0x22, 0x50, 0x51,
+        0x6c, 0x80, 0x53, 0xc3, 0xc2, 0xea, 0x92, 0x63,
+        0xf5, 0x1d, 0x17, 0xae, 0xdc, 0x37, 0xd3, 0x7e,
+        0x51, 0x54, 0x52, 0xb1, 0xfe, 0xf8, 0x04, 0x32};
+    Kuznyechik kuz;
+    kuz_new(&kuz, key);
+
+    kuz_ofb_encrypt(f_in, f_out, &kuz);
+    fseek(f_out, 0, SEEK_SET);
+
+    kuz_clear(&kuz);
+    kuz_new(&kuz, key);
+
+    kuz_ofb_encrypt(f_out, f_dec, &kuz);
+
+    kuz_clear(&kuz);
+    fclose(f_dec);
+    fclose(f_out);
+    fclose(f_in);
+}
+
+void encrypt_blocks_key_change(int cycles)
+{
+    const char *path_in = file_mil_blocks_in;
+    const char *path_out = file_mil_blocks_out;
+    const char *path_dec = file_mil_blocks_dec;
+
+    FILE *f_in = fopen(path_in, "rb");
+    if (f_in == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_in);
+        perror("fopen");
+        exit(1);
+    }
+    FILE *f_out = fopen(path_out, "w+b");
+    if (f_out == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_out);
+        perror("fopen");
+        exit(1);
+    }
+    FILE *f_dec = fopen(path_dec, "wb");
+    if (f_dec == NULL)
+    {
+        fprintf(stderr, "fopen: %s\n", path_dec);
+        perror("fopen");
+        exit(1);
+    }
+
+    kuz_key_t key = {0}, old_key = {0};
+    // Получаем ключ из /dev/urandom
+    kuz_clear_buf(key, 32);
+    memcpy(old_key, key, 32);
+    Kuznyechik kuz;
+    kuz_new(&kuz, key);
+
+    kuz_ofb_encrypt_key_change(f_in, f_out, &kuz, cycles, key);
+    fseek(f_out, 0, SEEK_SET);
+
+    kuz_clear(&kuz);
+    kuz_new(&kuz, old_key);
+
+    kuz_ofb_encrypt_key_change(f_out, f_dec, &kuz, cycles, old_key);
+
+    kuz_clear(&kuz);
+    fclose(f_dec);
+    fclose(f_out);
+    fclose(f_in);
+}
+
+void run_test_suite()
+{
+    // encrypt_1Mb();
+    // encrypt_100Mb();
+    // encrypt_1000Mb();
+    encrypt_blocks_key_change(10);
+    // encrypt_blocks_key_change(100);
+    // encrypt_blocks_key_change(1000);
+}
+
+int main()
+{
+    run_test_suite();
+    return 0;
+}
